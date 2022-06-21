aff99a58382f68d99a9a6d49b904e1d9c647f4bf
mkolumb <12272082+mkolumb@users.noreply.github.com>
2022-06-22 01:18:11 +0200

    Add DbContext extensions

---
+++ EFCore.Extensions.SaveOptimizer/EFCore.Extensions.SaveOptimizer/Extensions/DbContextExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCore.Extensions.SaveOptimizer.Extensions;
+
+public static class DbContextExtensions
+{
+    public static int SaveChangesOptimized(this DbContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static int SaveChangesOptimized(this DbContext context, bool acceptAllChangesOnSuccess)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static async Task<int> SaveChangesOptimizedAsync(this DbContext context, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static async Task<int> SaveChangesOptimizedAsync(this DbContext context, bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
