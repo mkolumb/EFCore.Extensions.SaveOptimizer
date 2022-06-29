e66947381871c418ceb53b49eb0289cfacc55c3d
mkolumb <12272082+mkolumb@users.noreply.github.com>
2022-06-29 09:46:12 +0200

    More rows for SQLite and Oracle

--- EFCore.Extensions.SaveOptimizer/EFCore.Extensions.SaveOptimizer.SqlLite.Benchmark/Specific/Variables.cs
+++ EFCore.Extensions.SaveOptimizer/EFCore.Extensions.SaveOptimizer.SqlLite.Benchmark/Specific/Variables.cs
@@ -4,5 +4,5 @@ public class Variables
 {
     public const string DbName = "SqlLite";
 
-    public static long[] Rows { get; } = { 1L, 10L, 25L, 50L, 100L, 1000L };
+    public static long[] Rows { get; } = { 1L, 10L, 25L, 50L, 100L, 1000L, 10000L };
 }
