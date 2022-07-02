464c4ac904899d38a3d8b0675a53bd9ba91bbe96
mkolumb <12272082+mkolumb@users.noreply.github.com>
2022-07-02 13:35:00 +0200

    FAKE build

---
+++ build.fsx
@@ -0,0 +1,24 @@
+#r "paket:
+nuget Fake.DotNet.Cli
+nuget Fake.IO.FileSystem
+nuget Fake.Core.Target //"
+#load ".fake/build.fsx/intellisense.fsx"
+
+open Fake.Core
+open Fake.DotNet
+open Fake.IO
+open Fake.IO.FileSystemOperators
+open Fake.IO.Globbing.Operators
+open Fake.Core.TargetOperators
+
+Target.initEnvironment ()
+
+Target.create "Clean" (fun _ -> !! "**/bin" ++ "**/obj" |> Shell.cleanDirs)
+
+Target.create "SolutionBuild" (fun _ -> !! "**/*.sln" |> Seq.iter (DotNet.build id))
+
+Target.create "All" ignore
+
+"Clean" ==> "SolutionBuild" ==> "All"
+
+Target.runOrDefault "All"
