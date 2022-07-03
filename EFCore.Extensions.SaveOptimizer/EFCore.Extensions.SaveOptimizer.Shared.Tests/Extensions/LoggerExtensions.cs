a08abb822c7ac57642139442adeaf4c7215bf8a3
mkolumb <12272082+mkolumb@users.noreply.github.com>
2022-07-03 09:54:40 +0200

    Split extensions

--- EFCore.Extensions.SaveOptimizer/EFCore.Extensions.SaveOptimizer.Shared.Tests/Extensions/LoggerExtensions.cs
+++ EFCore.Extensions.SaveOptimizer/EFCore.Extensions.SaveOptimizer.Shared.Tests/Extensions/LoggerExtensions.cs
@@ -1,40 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Logging;
 
 namespace EFCore.Extensions.SaveOptimizer.Shared.Tests.Extensions;
 
 public static class LoggerExtensions
 {
-    private const int RunTry = 10;
-
     public static void LogWithDate(this ILogger logger, string? text)
     {
         DateTime date = DateTime.Now;
 
         logger.Log(LogLevel.Warning, "[{date:HH:mm:ss}] {text}", date, text);
-    }
-
-    public static async Task<T[]> ToArrayWithRetry<T>(this IQueryable<T> source) =>
-        await Run(RunTry, () => source.ToArrayAsync());
-
-    private static async Task<T> Run<T>(int max, Func<Task<T>> method)
-    {
-        var i = 0;
-
-        while (i < max)
-        {
-            try
-            {
-                return await method();
-            }
-            catch
-            {
-                await Task.Delay(TimeSpan.FromSeconds(2));
-
-                i++;
-            }
-        }
-
-        throw new Exception("Unable to run method");
     }
 }
---
+++ EFCore.Extensions.SaveOptimizer/EFCore.Extensions.SaveOptimizer.Shared.Tests/Extensions/QueryableExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCore.Extensions.SaveOptimizer.Shared.Tests.Extensions;
+
+public static class QueryableExtensions
+{
+    private const int RunTry = 10;
+
+    public static async Task<T[]> ToArrayWithRetry<T>(this IQueryable<T> source) =>
+        await Run(RunTry, () => source.ToArrayAsync());
+
+    private static async Task<T> Run<T>(int max, Func<Task<T>> method)
+    {
+        var i = 0;
+
+        while (i < max)
+        {
+            try
+            {
+                return await method();
+            }
+            catch
+            {
+                await Task.Delay(TimeSpan.FromSeconds(2));
+
+                i++;
+            }
+        }
+
+        throw new Exception("Unable to run method");
+    }
+}
