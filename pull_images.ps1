a91e133e2af982acf7a543f3fa5a33e16c83cdbf
mkolumb <12272082+mkolumb@users.noreply.github.com>
2022-07-03 22:51:23 +0200

    Pull in parallel

--- pull_images.ps1
+++ pull_images.ps1
@@ -1,8 +1,16 @@
-docker pull cockroachdb/cockroach:latest-v22.1
-docker pull jacobalberty/firebird:3.0
-docker pull jacobalberty/firebird:v4.0
-docker pull mariadb:10
-docker pull mysql:8-oracle
-docker pull container-registry.oracle.com/database/express:latest
-docker pull postgres:14.4
-docker pull mcr.microsoft.com/mssql/server:2019-latest
+$images = @('cockroachdb/cockroach:latest-v22.1',
+    'jacobalberty/firebird:3.0',
+    'jacobalberty/firebird:v4.0',
+    'mariadb:10',
+    'mysql:8-oracle',
+    'container-registry.oracle.com/database/express:latest',
+    'postgres:14.4',
+    'mcr.microsoft.com/mssql/server:2019-latest')
+
+$images | ForEach-Object -ThrottleLimit 10 -Parallel {
+    $img = $_
+
+    Write-Host "Pulling $($img)"
+
+    docker pull $img
+}
