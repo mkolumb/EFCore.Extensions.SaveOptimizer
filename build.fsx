c6bdc746c60076c86815407b5373d65f959f29be
mkolumb <12272082+mkolumb@users.noreply.github.com>
2022-07-03 12:25:34 +0200

    Update build

--- build.fsx
+++ build.fsx
@@ -1,7 +1,8 @@
 #r "paket:
 nuget Fake.DotNet.Cli
 nuget Fake.IO.FileSystem
-nuget Fake.Core.Target //"
+nuget Fake.Core.Target
+nuget Microsoft.PowerShell.SDK //"
 #load ".fake/build.fsx/intellisense.fsx"
 
 open Fake.Core
@@ -10,29 +11,74 @@ open Fake.IO
 open Fake.IO.FileSystemOperators
 open Fake.IO.Globbing.Operators
 open Fake.Core.TargetOperators
+open System.Management.Automation
 
 Target.initEnvironment ()
 
+let scriptRun =
+    fun path ->
+        Trace.log ("Running script: " + path)
+
+        PowerShell
+            .Create()
+            .AddScript("& '" + path + "'")
+            .Invoke()
+        |> Seq.iter (printfn "%O")
+
+let testRun =
+    fun path ->
+        Trace.log ("Running test: " + path)
+
+        DotNet.test (fun o -> o) path
+
+let buildRun =
+    fun path ->
+        Trace.log ("Running build: " + path)
+
+        DotNet.build (fun o -> o) path
+
 Target.create "Clean" (fun _ -> !! "**/bin" ++ "**/obj" |> Shell.cleanDirs)
 
-Target.create "SolutionBuild" (fun _ -> !! "**/*.sln" |> Seq.iter (DotNet.build id))
+Target.create "RemoveContainersBefore" (fun _ ->
+    let dir = Shell.pwd ()
+    let path = Path.combine dir "remove_containers.ps1"
+    scriptRun (path))
+
+Target.create "RemoveContainersAfter" (fun _ ->
+    let dir = Shell.pwd ()
+    let path = Path.combine dir "remove_containers.ps1"
+    scriptRun (path))
+
+Target.create "SolutionBuild" (fun _ -> !! "**/*.sln" |> Seq.iter (buildRun))
 
 Target.create "InternalTests" (fun _ ->
     !! "**/*Internal*.Tests.csproj"
-    |> Seq.iter (DotNet.test id))
+    |> Seq.iter (testRun))
 
 Target.create "DbTest" (fun _ ->
     (!! "**/*.Tests.csproj"
      -- "**/*Internal*.Tests.csproj"
      -- "**/*Shared*.Tests.csproj")
-    |> Seq.iter (DotNet.test id))
+    |> Seq.iter (fun job ->
+        let dir =
+            (FileInfo.ofPath (job.Replace("Tests", "Benchmark")))
+                .Directory
+                .FullName
+
+        scriptRun (Path.combine dir "start.ps1")
+
+        testRun (job)
+
+        scriptRun (Path.combine dir "stop.ps1")))
 
 Target.create "All" ignore
 
 "Clean"
+==> "RemoveContainersBefore"
 ==> "SolutionBuild"
 ==> "InternalTests"
 ==> "DbTest"
+==> "RemoveContainersAfter"
 ==> "All"
 
 Target.runOrDefault "All"
